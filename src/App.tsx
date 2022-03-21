32165e2b5914fc571b69a0b14fc1d8c7ccae2a2b
SATYAM PATHAK <satyampathaknewid@gmail.com>
2022-03-22 02:46:02 +0530

    FAQ Demo

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import Footer from "./sections/Footer";
 import Navbar from "./sections/Navbar";
 import Rankings from "./sections/Rankings";
 import Statistics from "./sections/Statistics";
+import Accordion from './sections/Accordion';
+import { accordionData } from './sections/content';
 
 function App() {
   return (
@@ -12,9 +14,20 @@ function App() {
       <Rankings />
       <Communique />
       <Statistics />
+      <div>
+      <div id="statistics" className='w-full flex justify-center'>
+        <div className='w-4/5 flex flex-col items-center'>
+            <h1 className='mb-4 text-4xl font-bold text-[#003865] leading-loosey'>FREQUENTLY ASKED QUESTIONS</h1>
+        </div>
+      </div>
+      <div className="accordion">
+          {accordionData.map(({ title, content }) => (
+            <Accordion title={title} content={content} />
+          ))}
+        </div>
+    </div>
       <Footer />
     </>
   );
 }
-
 export default App;
