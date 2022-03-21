c2fd38eca96128909284f4ce83c902e5006a1294
Ayushi Khandelia <khandeliaayushi08@gmail.com>
2022-03-22 00:49:38 +0530

    added about us section

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import Footer from './sections/Footer';
 import Navbar from './sections/Navbar';
+import AboutUs from './sections/aboutus';
 
 function App() {
   return <>
   <Navbar />
+  <AboutUs/>
   <Footer />
+  
   </>;
 }
 
