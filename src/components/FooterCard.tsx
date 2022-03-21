687ab00018995803d2deb7c4cc07a7bddb2dbb4f
Kartik4152 <gaming4152@gmail.com>
2022-03-21 21:07:00 +0530

    feat: added footer

--- src/components/FooterCard.tsx
+++ src/components/FooterCard.tsx
@@ -1,8 +1,18 @@
 import React from 'react'
 
-const FooterCard = () => {
+const FooterCard:React.FC<{icon: string, title:string, text: string}> = ({icon, title, text}) => {
   return (
-    <div>FooterCard</div>
+    <div className='flex'>
+      <img src={icon} alt="google map pin" className='w-12 h-12 object-contain mr-2'/>
+      <div className='flex flex-col max-w-[10rem]'>
+        <div className='text-white'>
+          {title}
+        </div>
+        <div className='text-white text-xs'>
+          {text}
+        </div>
+      </div>
+    </div>
   )
 }
 
--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -1,22 +1,39 @@
 import React from 'react'
 import FooterCard from '../components/FooterCard'
-import QrCode from "../assets/qr.png";
+import QrCode from "../assets/qr.png"
+import Logo_Pin from "../assets/location_icon.png"
+import Logo_Phone from "../assets/contact_icon.png"
+import Logo_Mail from "../assets/mail_icon.png"
+import Logo_Instagram from "../assets/instagram_icon.png";
+import Logo_Linkedin from "../assets/linkedin_icon.png";
+import Logo_Twitter from "../assets/twitter_icon.png";
+
 const Footer = () => {
   return (
     <div id="footer" className='bg-[#242424] w-full flex flex-col items-center'>
-        <div className='w-4/5'>
-            <div>
-                <FooterCard />
-                <FooterCard />
-                <FooterCard />
+        <div className='w-3/5 py-12 text-white'>
+            <div className='flex items-center w-full justify-between border-b-4 pb-8 px-8 mb-8 border-[#777777]'>
+                <FooterCard title="FIND US" icon={Logo_Pin} text="Dean Office, USICT, GGSIPU Sector 16C, Dwarka, Delhi"/>
+                <FooterCard title="CALL US" icon={Logo_Phone} text="011-25302843"/>
+                <FooterCard title="MAIL US" icon={Logo_Mail} text="info@tnp.ac.in"/>
             </div>
-            <div>
-              <div></div>
-              <div></div>
-              <img src={QrCode} alt="qr code"/>
+            <div className='flex justify-between w-full'>
+              <div className='flex flex-col space-y-4'>
+                <h1 className='uppercase text-2xl'>Quick Links</h1>
+                <span>&gt; Procedure</span>
+                <span>&gt; Recruiters</span>
+                <span>&gt; Statistics</span>
+              </div>
+              <div className='flex flex-col space-y-4'>
+                <h1 className='uppercase text-2xl'>Social Links</h1>
+                <span className='flex'><img src={Logo_Instagram} alt="social logo" className='w-6 h-6 object-contain mr-4' />Instagram</span>
+                <span className='flex'><img src={Logo_Linkedin} alt="social logo" className='w-6 h-6 object-contain mr-4' />Linkedin</span>
+                <span className='flex'><img src={Logo_Twitter} alt="social logo" className='w-6 h-6 object-contain mr-4' />Twitter</span>
+              </div>
+              <img src={QrCode} alt="qr code" className='w-64 h-64 object-contain object-right'/>
             </div>
         </div>
-        <div className='bg-[#2D2D2D] w-full flex items-center justify-center text-white py-4'>
+        <div className='bg-[#2D2D2D] w-full flex items-center justify-center text-white p-4 py-8'>
             © Conceptualized, designed and developed by office bearers of Training and Placement Cell
         </div>
     </div>
