74df3a3f35799081f497efddcd230f4b125cb034
Kartik4152 <gaming4152@gmail.com>
2022-03-21 21:51:26 +0530

    feat: added rankings

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import React from "react";
 import Communique from "./sections/Communique";
 import Footer from "./sections/Footer";
 import Navbar from "./sections/Navbar";
+import Rankings from "./sections/Rankings";
 
 function App() {
   return (
     <>
       <Navbar />
+      <Rankings />
       <Communique />
       <Footer />
     </>
---
+++ src/sections/Statistics.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Statistics = () => {
+  return (
+    <div id="statistics">
+    </div>
+  )
+}
+
+export default Statistics
