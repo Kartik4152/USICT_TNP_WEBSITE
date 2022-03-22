f476d8bd192471eef282a00972c64d0339b7bfd4
MRIDUL-MB <mriravbagla@gmail.com>
2022-03-22 09:38:16 +0530

    header section

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Footer from './sections/Footer';
 import Navbar from './sections/Navbar';
+import Header from './sections/Header';
 
 function App() {
   return <>
   <Navbar />
+  <Header/>
   <Footer />
   </>;
 }
