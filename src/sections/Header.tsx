4a4c2ca04633c8a2197cb5c58fff2e801e3eb5f2
Kartik4152 <gaming4152@gmail.com>
2022-03-22 17:30:04 +0530

    fix: updated header z-index

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -17,7 +17,7 @@ export default function Header(){
           <img src={require('../assets/header-1.jpg')} alt="header-1" className='object-fill h-full w-full brightness-50'/>
       </SwiperSlide>
     </Swiper>
-    <div className='grid grid-cols-4 gap-10 absolute z-50 m-auto top-1/3 right-64 left-64 bottom-1/3 text-white'>
+    <div className='grid grid-cols-4 gap-10 absolute z-10 m-auto top-1/3 right-64 left-64 bottom-1/3 text-white'>
         <div className='col-span-2  grid grid-cols-3  '>
             <i className="text-6xl fa-solid fa-indian-rupee-sign text-yellow-400"></i>
             <h1 className='text-4xl col-span-2 '>HIGHEST PACKAGE:<br/> RS 32 LPA</h1>
