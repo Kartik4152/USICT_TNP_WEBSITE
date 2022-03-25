6ac2d78fed5bc9505821c5c758e5f851cdfcd5c3
manikarora8368 <manikarora8368@gmail.com>
2022-03-25 20:47:36 +0530

    updated about us section

--- src/sections/AboutUs.tsx
+++ src/sections/AboutUs.tsx
@@ -1,12 +1,12 @@
 const AboutUs = () => {
   return (
     <>
-      <div className="bg-stone-900/75 flex h-100 max-h-full  w-full">
+      <div id="about_us" className=" flex w-full ">
         <p>
-          <h3 className="font-mono text-white font-bold text-2xl text-center mt-6 underline underline-offset-4 ">
+          <h3 className="font-mono text-[#003865] font-bold text-2xl text-center mt-6 underline underline-offset-4 ">
             About Us
           </h3>
-          <p className="font-mono text-white mx-64 my-4">
+          <p className="font-mono text-[#366286] mx-64 my-8 py-4">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
             eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
             ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
